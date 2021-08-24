80781f82cfde6f745d36b9d0167866a5e4d36ae2
DmitryS1989 <movs1989@gmail.com>
2021-08-24 21:37:54 +0300

    Finish static page

--- spec/requests/static_pages_controller_spec.rb
+++ spec/requests/static_pages_controller_spec.rb
@@ -1,31 +1,16 @@
 require 'rails_helper'
 
+
 RSpec.describe 'StaticPagesControllers', type: :request do
 
-  describe 'GET /home' do
-    it 'should get home' do
-      get '/home'
-      expect(response.status).to eq(200)
-      expect(response).to render_template('home')
-      expect(response).to have_title('Home | Ruby on Rails Tutorial Sample App', exact: true)
+  def self.should_get_static_page(*links)
+    links.each do |a|
+      it "#{a} have status 200" do
+        get a
+        expect(response.status).to eq(200)
+        expect(response).to render_template(a[1..])
+      end
     end
   end
-  describe 'GET /help' do
-    it 'should get help ' do
-      get '/help'
-      expect(response.status).to eq(200)
-      expect(response).to render_template('help')
-      expect(response).to have_title('Home | Ruby on Rails Tutorial Sample App', exact: true)
-
-    end
-  end
-  describe 'GET /about' do
-    it 'should get about ' do
-      get '/about'
-      expect(response.status).to eq(200)
-      expect(response).to render_template('about')
-      expect(response).to have_title('About | Ruby on Rails Tutorial Sample App', exact: true)
-    end
-
-  end
+  should_get_static_page '/home', '/help', '/about'
 end
