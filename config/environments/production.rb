21e3fbf6a0aeb3007db018046cb011a0e25485b5
DmitryS1989 <movs1989@gmail.com>
2021-08-26 22:14:40 +0300

    aa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "myapp_production"
-
-  config.action_mailer.default_url_options = { host: 'https://damp-brook-50173.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://damp-brook-50173.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
