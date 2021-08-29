f15593210302242a523d5cc64f77e334a2e0c682
DmitryS1989 <movs1989@gmail.com>
2021-08-29 23:07:07 +0300

    fixed routes in confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                                    confirmations: 'confirmations' }
 
   get 'users/new'
   get 'home' => 'static_pages#home'
