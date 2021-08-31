723bfaecca8cd8d9c8873c46b4bde42562c070eb
DmitryS1989 <movs1989@gmail.com>
2021-08-31 13:43:30 +0300

    added name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable,
          omniauth_providers: [:google_oauth2]
 
+
+
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
@@ -12,6 +14,7 @@ class User < ApplicationRecord
     # Uncomment the section below if you want users to be created if they don't exist
     user ||= User.create(
       email: data['email'],
+      name: data['name'],
       password: Devise.friendly_token[0, 20]
     )
     user
